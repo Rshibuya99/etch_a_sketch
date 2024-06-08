dc9d92addfaf0a3bab2b45a52ae5a01ce6482577
Rocco Shibuya <shibuyaryouken@gmail.com>
2024-06-08 23:00:37 +0800

    attach hovering effect and randomize color

--- script.js
+++ script.js
@@ -10,26 +10,37 @@ window.addEventListener("DOMContentLoaded", function() {
     const SIZE_INPUT        = document.createElement("input")
     const SIZE_BUTTON       = document.createElement("button");
 
-    const N_BOX_GAP         = 4;
+    const ORIGINAL_SIZE     = 16;
+
 
     // style DOM elements
     BODY.setAttribute("style",
-    "display: flex; flex: auto; justify-content: center; align-items: center; flex-direction: column;")
+    "display: flex; justify-content: center; align-items: center; flex-direction: column;")
     MAIN_CONTAINER.setAttribute("style",
-    `display: flex; flex-direction: column; gap: ${N_BOX_GAP}px; padding: 45px 100px;`);
+    `display: flex; flex-direction: column; padding: 18px 100px; width: 700px; `);
     ROW_CONTAINER.setAttribute("style",
-    `display: flex; flex-direction: row; gap: ${N_BOX_GAP}px;`);
+    `display: flex; flex-direction: row; justify-content: space-evenly;`);
     BOX.setAttribute("style",
-    "width: 25px; height: 25px; border: solid 1px black;");
+    "width: 25px; aspect-ratio: 1/1; border: solid 1px grey; flex-grow: 1;");
     BOX.classList.add("hoverBox")
 
     SIZE_FORM.setAttribute("style",
-    "display: flex; flex-direction: row; flex: auto; justify-content: space-evenly; align-items: flex-end; gap: 10px; margin-top: 30px;")
+    "display: flex; flex-direction: row; flex: auto; justify-content: space-evenly; align-items: flex-end; gap: 10px; margin-top: 10px;")
     SIZE_LABEL.textContent = "Input Size: "
     SIZE_BUTTON.textContent = "Confirm"
     SIZE_FORM.id = "size-form"
     SIZE_INPUT.id = "size-form-n"
+    SIZE_INPUT.type = "text"
+    SIZE_INPUT.name = "input"
+    SIZE_INPUT.placeholder = "1 - 100"
+    SIZE_INPUT.autocomplete = "off"
+    SIZE_BUTTON.type = "submit"
 
+    SIZE_INPUT.addEventListener("focus", function() {
+        
+        SIZE_INPUT.placeholder = "";
+    
+    })
     BODY.insertBefore(SIZE_FORM, BODY.children[0]);
     SIZE_FORM.appendChild(SIZE_LABEL)
     SIZE_LABEL.appendChild(SIZE_INPUT)
@@ -37,25 +48,65 @@ window.addEventListener("DOMContentLoaded", function() {
 
 
     // initialize 16x16 board
-    for (let i = 0; i < 16; i++) {
-        ROW_CONTAINER.appendChild(BOX.cloneNode(true));
-    }
-    for (let i = 0; i < 16; i++) {
-        MAIN_CONTAINER.appendChild(ROW_CONTAINER.cloneNode(true))
-    }
+    createBoard(MAIN_CONTAINER, ROW_CONTAINER, BOX, ORIGINAL_SIZE);
+
 
     // attach hover effects
     MAIN_CONTAINER.addEventListener("mouseover", function(e){
         if (e.target.classList.contains("hoverBox")) {
-            e.target.style.backgroundColor = "black";
+            color = randomizeColor();
+            console.log(color);
+            e.target.style.backgroundColor = randomizeColor();
         }
     })
 
+
     // alter number of boxes
-    SIZE_FORM.addEventListener("submit", function() {
-        console.log(SIZE_LABEL.textContent)
+    SIZE_FORM.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        let numberOfBoxes = evaluateValue(SIZE_INPUT.value, ORIGINAL_SIZE);
+        createBoard(MAIN_CONTAINER, ROW_CONTAINER, BOX, numberOfBoxes);
+
+        SIZE_FORM.reset();
+        SIZE_INPUT.blur();
     })
 
+    console.log(randomizeColor());
     
 })
 
+
+createBoard = function(main_container, row_container, box, box_size) {
+    main_container.innerHTML = '';
+    row_container.innerHTML = '';
+
+    for (let i = 0; i < box_size; i++) {
+        row_container.appendChild(box.cloneNode(true))
+    }
+    for (let i = 0; i < box_size; i++) {
+        main_container.appendChild(row_container.cloneNode(true))
+    }
+}
+
+
+evaluateValue = function(text, original_size) {
+    let size = parseInt(text);
+    if (isNaN(size)) {
+        return original_size;
+    }
+    else if (size > 100 || size < 1) {
+        return original_size;
+    }
+    else {
+        return size
+    }
+}
+
+randomizeColor = function() {
+    red = Math.floor(Math.random() * 255)
+    green = Math.floor(Math.random() * 255)
+    blue = Math.floor(Math.random() * 255)
+
+    return(`rgb(${red}, ${green}, ${blue})`)
+}
